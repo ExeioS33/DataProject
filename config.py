9b734512df4b83aa9f4058545a082202d2b18a7e
Exeio <andrirazafy9@gmail.com>
2024-05-29 19:26:39 +0200

    correct credentials vulnerability

--- config.py
+++ config.py
@@ -14,4 +14,17 @@ TICKERS = ['AI.PA','AIR.PA','ALO.PA','MT.AS','CS.PA','BNP.PA','EN.PA','CAP.PA','
                'SAN.PA', 'SU.PA', 'GLE.PA', 'STLAP.PA', 'STMPA.PA', 'TEP.PA', 'HO.PA', 'TTE.PA', 'URW.PA', 'VIE.PA',
                'DG.PA', 'WLN.PA'
                ] 
+
 SAVE_DIRECTORY = "./financial_data_lake"
+
+SAVE_EXCEL = "./intraday_directory"
+
+EMAIL_CONFIG = {
+    "sender_email": "sandampitiaandria@gmail.com",
+    "receiver_email": "razafimahatratra.andriamandimbisoambelomasina@supdevinci-edu.fr",
+    "smtp_server": "smtp.office365.com",
+    "smtp_port": 587,
+    "smtp_user": "sandampitiaandria@gmail.com",
+    "smtp_password": "your password"
+}
+
